e9fa036d107a4dec5f4096d560a4d78012d4339a
Jose Monge <49077791+josemonge01@users.noreply.github.com>
2019-04-12 17:46:40 -0400

    Add files via upload

---
+++ User.cpp
@@ -0,0 +1,17 @@
+//
+//  User.cpp
+//  
+//
+//  Created by Jose Monge on 4/12/19.
+//
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+class User {
+    
+private:
+    string userID;
+    string password;
+};
