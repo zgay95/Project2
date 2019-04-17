3973afc50ce8da24c2cdac45328983afdcf9713e
Zachary Gay <47303226+zgay95@users.noreply.github.com>
2019-04-17 14:45:12 -0400

    Update Advisor.h

--- Advisor.h
+++ Advisor.h
@@ -12,21 +12,29 @@
 #include <string>
 #include <vector>
 #include "Student.h"
+#include "User.h"
 
 using namespace std;
 
-class Advisor : public User {
+class Advisor : User {
 private:
 	string ID;
+	string password;
 	string fName;
 	string lName;
 	string room;
 	string phone_num;
 	vector<Student> advisees;
 public:
-	Advisor(string ID, string fname, string lname, string room, string phone, vector<Student> ad);
+	Advisor(string ID, string pw, string fname, string lname, string room, string phone, vector<Student> ad);
+	string GetID();
+	string GetPassword();
+	string GetFirstName();
+	string GetLastName();
+	string GetRoom();
+	string GetPhoneNum();
+	string GetAdvisees();
 	void printMenu();
-	void handleMenuSelection(int selection);
 	void printAdviseeList();
 	void search();
 	void printNotesMenu();
