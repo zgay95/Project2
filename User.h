98b356683ff799decf8d2f0557209da3ba66e0d2
Zachary Gay <47303226+zgay95@users.noreply.github.com>
2019-04-17 14:47:54 -0400

    Add files via upload

---
+++ User.h
@@ -0,0 +1,20 @@
+//
+//  User.cpp
+//  
+//
+//  Created by Jose Monge on 4/12/19.
+//
+#ifndef USER_H
+#define USER_H
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+class User {
+
+private:
+	string userID;
+	string password;
+
+};
+#endif 
