6e409c99a34caeaf797bd11abe09c3fd73a1a064
jack98yang <49003547+jack98yang@users.noreply.github.com>
2019-04-19 16:21:43 -0400

    Create User.cpp

---
+++ User.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+#include "User.h"
+
+using namespace std;
+
+User::User(string a, string b) {
+	userID = a;
+	password = b;
+}
+string User::GetuserID() {
+	return userID;
+}
+string User::Getpasswork() {
+	return password;
+}
